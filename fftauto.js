d73b6f96e4b52925f9e74f08dd1db30fdcfb74b6
dimashalim7 <34086249+dimashalim7@users.noreply.github.com>
2017-11-29 21:34:28 +0700

    Update fftauto.js

--- fftauto.js
+++ fftauto.js
@@ -123,10 +123,10 @@ const Excute = async function(User, TargetUsername, TextComment){
 						console.log(chalk`{bold.red [-|Comment Failed]} ({bold.cyan ${account.username}}) ${account.id} => GAGAL`);
 					}
 				}));
-				console.log(chalk`{bold.yellow [!]} Delay For 180000 Milisecond (3 Minute) To Next Block`);
+				console.log(chalk`{bold.yellow [!]} Delay For 120000 Milisecond (3 Minute) To Next Block`);
 				await delay(180000);
 			}
-			console.log(chalk`{bold.yellow [!]} Delay For 300000 Milisecond (5 Minute) To Next Cursor`);
+			console.log(chalk`{bold.yellow [!]} Delay For 30000 Milisecond (5 Minute) To Next Cursor`);
 			await delay(300000);
 		}
 		while(cursor != null);
